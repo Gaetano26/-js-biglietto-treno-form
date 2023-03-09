74bc2b8e0448daf53860c8f79e11b3139a3424c3
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-09 17:38:07 +0100

    add condition

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
 
 const app = document.getElementById('app');
 
-const nomeCognomeBox = app.querySelector('input[name="nome-cognome]');
+const nomeCognomeBox = app.querySelector('input[name="nome-cognome"]');
 
 const ageBox = app.querySelector('input[name="age"]');
 
@@ -32,10 +32,12 @@ const nominativo = app.querySelector('.nominativo')
 
 
 generate.addEventListener('click', function (){
-    let nomeCognome = nomeCognomeBox;
+    let nomeCognome = nomeCognomeBox.value;
     let age = ageBox.value;
     let km = kmBox.value;
     let priceForKm = 0.21;
+    let discountForMinors = 0.20;
+    let discountOver65 = 0.40;
     let price = (km * priceForKm).toFixed(2);
     ticket.innerHTML += `
     <h4>${price} euro</h4>
@@ -43,11 +45,33 @@ generate.addEventListener('click', function (){
     nominativo.innerHTML += `
     <h4>${nomeCognome}</h4>
     `;
+    if (age < 18 ){
+        price = (price -(price * discountForMinors)).toFixed(2);
+
+       ticket.innerHTML += 
+       `
+      <h4> ${price} euro </h4> 
+       `;
+   // Condizione sconto superiore ai 65 anni
+      } else if (age > 65 ) {
+      price = (price - (price * discountOver65)).toFixed(2);
+      ticket.innerHTML += 
+      `
+      <h4> ${price} </h4>
+       `;
+  // Condizione prezzo normale senza sconto
+  } else {
+    ticket.innerHTML += 
+      `
+      <h4>${price} euro </h4>
+      `;
+  }
+
 })
 
 
 clear.addEventListener('click', function (){
-    nomeCognomeBox.textContent = '';
+    nomeCognomeBox.value = '';
     ageBox.value = '';
     kmBox.value = '';
     ticket.innerHTML = '';
